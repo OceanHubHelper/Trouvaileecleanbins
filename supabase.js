788c332a57a6881960ac16b3ffffdc32e95dcf74
OceanHubHelper <gonzalez_mya1@icloud.com>
2026-04-21 19:17:10 -0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// 🔥 Replace these with your real Supabase project keys
+const SUPABASE_URL = "https://haxotlqsasigorctuvil.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_9r0DEvw-b_qK_aUqjA2AEQ_S7clkBfb";
+
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
